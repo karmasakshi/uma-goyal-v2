087607fcd3f571529ed8a38a0b1ca9703084744c
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-07-25 01:32:27 +0400

    feat: add chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import '@styles/global.scss';
 import { AppProps } from "next/app";
 import { FunctionComponent, ReactElement } from "react";
 
 const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps): ReactElement => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
