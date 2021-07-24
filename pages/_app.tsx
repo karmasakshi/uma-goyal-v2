8729469d42c1240024071b097dd4049765c5ba89
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-07-24 20:49:30 +0400

    feat: add typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from "next/app";
+import { FunctionComponent, ReactElement } from "react";
+
+const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps): ReactElement => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent, ReactElement } from "react";
+
+const Index: FunctionComponent = (): ReactElement => (
+  <div>Welcome to Next.js!</div>
+);
+
+export default Index;
